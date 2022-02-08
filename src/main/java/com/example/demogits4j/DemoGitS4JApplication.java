24e4659e54c898036c9cb022fb2f63e65d957ee1
Oana Flamanzanu <oana.flamanzanu@endava.com>
2022-02-08 17:44:20 +0200

    Initial commit

---
+++ src/main/java/com/example/demogits4j/DemoGitS4JApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogits4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitS4JApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitS4JApplication.class, args);
+    }
+
+}
